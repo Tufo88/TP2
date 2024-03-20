4f954c12dab03877c881c38230643c4a2c21e269
Tufo88 <55107276+Tufo88@users.noreply.github.com>
2024-03-20 11:22:58 +0100

    Update SpeciesTableModel.java

--- tp2/src/simulator/view/SpeciesTableModel.java
+++ tp2/src/simulator/view/SpeciesTableModel.java
@@ -1,6 +1,7 @@
 
 public class SpeciesTableModel extends AbstractTableModel implements EcoSysObserver {
-	List</*XD*/> _data;
+	Map<String, Map<State, Integer>> _data;//mapa de genetic code a mapa de state a num de animales en ese state
+	
 	private static List<String> columnNames;
 
 	SpeciesTableModel(Controller ctrl) {
